84ed54632898f11a3bd7a68312f462f23ffe9e34
Sean Lingren <lingrino@github.com>
2020-01-29 13:46:53 -0600

    randomInjector

--- fault.go
+++ fault.go
@@ -132,6 +132,12 @@ type Injector interface {
 	Handler(next http.Handler) http.Handler
 }
 
+// ChainInjector combines many injectors into a single chain injector. In a chain injector the
+// Handler func will execute ChainInjector.middlewares in order and then returns.
+type ChainInjector struct {
+	middlewares []func(next http.Handler) http.Handler
+}
+
 // NewChainInjector combines many injectors into a single chain injector. In a chain injector
 // the Handler() for each injector will execute in the order provided.
 func NewChainInjector(is ...Injector) (*ChainInjector, error) {
@@ -141,12 +147,6 @@ func NewChainInjector(is ...Injector) (*ChainInjector, error) {
 	}
 
 	return chainInjector, nil
-}
-
-// ChainInjector combines many injectors into a single chain injector. In a chain injector the
-// Handler func will execute ChainInjector.middlewares in order and then returns.
-type ChainInjector struct {
-	middlewares []func(next http.Handler) http.Handler
 }
 
 // Handler executes ChainInjector.middlewares in order and then returns.
@@ -159,6 +159,36 @@ func (i *ChainInjector) Handler(next http.Handler) http.Handler {
 			}
 		}
 		next.ServeHTTP(w, r)
+	})
+}
+
+// NewRandomInjector combines many injectors into a single random injector. When the random injector
+// is called it randomly runs one of the provided injectors.
+func NewRandomInjector(is ...Injector) (*RandomInjector, error) {
+	RandomInjector := &RandomInjector{randF: rand.Intn}
+
+	for _, i := range is {
+		RandomInjector.middlewares = append(RandomInjector.middlewares, i.Handler)
+	}
+
+	return RandomInjector, nil
+}
+
+// RandomInjector combines many injectors into a single injector. When the random injector
+// is called it randomly runs one of the provided injectors.
+type RandomInjector struct {
+	randF       func(int) int
+	middlewares []func(next http.Handler) http.Handler
+}
+
+// Handler executes a random injector from RandomInjector.middlewares
+func (i *RandomInjector) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if i != nil && len(i.middlewares) > 0 {
+			i.middlewares[i.randF(len(i.middlewares))](next).ServeHTTP(w, r)
+		} else {
+			next.ServeHTTP(w, r)
+		}
 	})
 }
 
--- fault_test.go
+++ fault_test.go
@@ -568,6 +568,180 @@ func TestChainInjectorHandler(t *testing.T) {
 	}
 }
 
+// TestNewRandomInjector tests NewRandomInjector.
+func TestNewRandomInjector(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name    string
+		give    []Injector
+		wantLen int
+		wantNil bool
+		wantErr error
+	}{
+		{
+			name:    "nil",
+			give:    nil,
+			wantLen: 0,
+			wantNil: false,
+			wantErr: nil,
+		},
+		{
+			name:    "empty",
+			give:    []Injector{},
+			wantLen: 0,
+			wantNil: false,
+			wantErr: nil,
+		},
+		{
+			name: "one",
+			give: []Injector{
+				&SlowInjector{
+					duration: time.Millisecond,
+					sleep:    time.Sleep,
+				},
+			},
+			wantLen: 1,
+			wantNil: false,
+			wantErr: nil,
+		},
+		{
+			name: "two",
+			give: []Injector{
+				&SlowInjector{
+					duration: time.Millisecond,
+					sleep:    time.Sleep,
+				},
+				&ErrorInjector{
+					statusCode: http.StatusInternalServerError,
+					statusText: http.StatusText(http.StatusInternalServerError),
+				},
+			},
+			wantLen: 2,
+			wantNil: false,
+			wantErr: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			i, err := NewRandomInjector(tt.give...)
+
+			assert.Equal(t, tt.wantErr, err)
+			if tt.wantNil {
+				assert.Nil(t, i)
+			} else {
+				assert.Equal(t, tt.wantLen, len(i.middlewares))
+			}
+		})
+	}
+}
+
+// TestRandomInjectorHandler tests RandomInjector.Handler.
+func TestRandomInjectorHandler(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		give     *RandomInjector
+		wantCode int
+		wantBody string
+	}{
+		{
+			name:     "nil",
+			give:     nil,
+			wantCode: testHandlerCode,
+			wantBody: testHandlerBody,
+		},
+		{
+			name:     "empty",
+			give:     &RandomInjector{},
+			wantCode: testHandlerCode,
+			wantBody: testHandlerBody,
+		},
+		{
+			name: "nil middlewares",
+			give: &RandomInjector{
+				middlewares: nil,
+			},
+			wantCode: testHandlerCode,
+			wantBody: testHandlerBody,
+		},
+		{
+			name: "empty middlewares",
+			give: &RandomInjector{
+				middlewares: []func(next http.Handler) http.Handler{},
+			},
+			wantCode: testHandlerCode,
+			wantBody: testHandlerBody,
+		},
+		{
+			name: "one",
+			give: &RandomInjector{
+				randF: func(int) int { return 0 },
+				middlewares: []func(next http.Handler) http.Handler{
+					func(next http.Handler) http.Handler {
+						return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+							w.WriteHeader(http.StatusOK)
+							fmt.Fprint(w, "one")
+							next.ServeHTTP(w, r)
+						})
+					},
+				},
+			},
+			wantCode: http.StatusOK,
+			wantBody: "one" + testHandlerBody,
+		},
+		{
+			name: "two",
+			give: &RandomInjector{
+				randF: func(int) int { return 1 },
+				middlewares: []func(next http.Handler) http.Handler{
+					func(next http.Handler) http.Handler {
+						return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+							w.WriteHeader(http.StatusOK)
+							fmt.Fprint(w, "one")
+							next.ServeHTTP(w, r)
+						})
+					},
+					func(next http.Handler) http.Handler {
+						return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+							w.WriteHeader(http.StatusOK)
+							fmt.Fprint(w, "two")
+							next.ServeHTTP(w, r)
+						})
+					},
+				},
+			},
+			wantCode: http.StatusOK,
+			wantBody: "two" + testHandlerBody,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			f := &Fault{
+				opt: Options{
+					Enabled:           true,
+					Injector:          tt.give,
+					PercentOfRequests: 1.0,
+				},
+			}
+
+			rr := testRequest(t, f)
+
+			assert.Equal(t, tt.wantCode, rr.Code)
+			assert.Equal(t, tt.wantBody, strings.TrimSpace(rr.Body.String()))
+		})
+	}
+}
+
 // TestNewRejectInjector tests NewRejectInjector.
 func TestNewRejectInjector(t *testing.T) {
 	t.Parallel()
